f106baa4ecbe0df99464e25c02e1371ae4a467e0
mahee96 <47920326+mahee96@users.noreply.github.com>
2026-08-31 11:45:43 +0530

    pin down swift-crypto version

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "5.0.0")
+        .package(url: "https://github.com/apple/swift-crypto.git", exact: "4.3.1")
     ],
     targets: [
         .binaryTarget(
